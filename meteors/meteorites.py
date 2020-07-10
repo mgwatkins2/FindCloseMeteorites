13abb24a799ffd778e922d3242a8bfb7e022c292
Matthew Watkins <mgwatkins2@gmail.com>
2020-07-10 19:20:59 -0400

    Dont run script code when imported.

--- meteors/meteorites.py
+++ meteors/meteorites.py
@@ -17,18 +17,19 @@ def calc_dist(lat1, lon1, lat2, lon2):
 def get_dist(meteor):
     return meteor.get('distance', math.inf)
 
-my_loc = (float(input("Your latitude: ")), float(input("Your longitude: ")))
+if __name__ == '__main__' :
+    my_loc = (float(input("Your latitude: ")), float(input("Your longitude: ")))
+    
+    meteor_resp = requests.get('https://data.nasa.gov/resource/gh4g-9sfh.json')
+    meteor_data = meteor_resp.json()
 
-meteor_resp = requests.get('https://data.nasa.gov/resource/gh4g-9sfh.json')
-meteor_data = meteor_resp.json()
+    for meteor in meteor_data:
+        if not ('reclat' in meteor and 'reclong' in meteor): continue
+        meteor['distance'] = calc_dist(float(meteor['reclat']),
+                            float(meteor['reclong']),
+                            my_loc[0],
+                            my_loc[1])
 
-for meteor in meteor_data:
-    if not ('reclat' in meteor and 'reclong' in meteor): continue
-    meteor['distance'] = calc_dist(float(meteor['reclat']),
-                                   float(meteor['reclong']),
-                                   my_loc[0],
-                                   my_loc[1])
-                    
-meteor_data.sort(key=get_dist)
+    meteor_data.sort(key=get_dist)
 
-print(meteor_data[0:10])
+    print(meteor_data[0:10])
